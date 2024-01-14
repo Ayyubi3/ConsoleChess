7240e994325755746ca2646a64896ad32f3971bf
Eyup3 <eyup.ozturk@outlook.de>
2024-01-14 20:18:47 +0100

    reset

--- ConsoleChess/src/Engine.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Point.h"
-
-void ENGINE_SetBackgroundColor(int R, int G, int B);
-void ENGINE_SetForegroundColor(int R, int G, int B);
-void ENGINE_SetCursorPos(Point coords);
